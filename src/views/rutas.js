9f92120beb3cb30e8c43441e6baf3c86e1f73942
EzPlays <ezapata113@misena.edu.co>
2022-10-25 15:14:17 -0500

    local y host comentado

--- src/views/rutas.js
+++ src/views/rutas.js
@@ -1,4 +1,4 @@
-/* rutas api */
+/* rutas api local*/
 const baseurl = "http://localhost:5001"
 
 //rutas usuario
@@ -18,6 +18,27 @@ const carritoDelete = "http://localhost:5001/carrito/"
 //rutas mis_compras
 const comprasGetAndPost = "http://localhost:5001/compras"
 const comprasGetAndDelete = "http://localhost:5001/compras/"
+
+// /* rutas api host*/
+// const baseurl = "https://apitiendavirtual-production.up.railway.app"
+
+// //rutas usuario
+// const usuariosGetAndPost = "https://apitiendavirtual-production.up.railway.app/usuarios"
+// const usuariosDeleteAndPut = "https://apitiendavirtual-production.up.railway.app/usuarios/"
+// const usuario_login = "https://apitiendavirtual-production.up.railway.app/usuarios/login"
+
+// //rutas productos
+// const productosGetAndPost = "https://apitiendavirtual-production.up.railway.app/productos"
+// const productosDeleteAndPut = "https://apitiendavirtual-production.up.railway.app/productos/"
+
+// //rutas carrito
+// const carritoGetAndPost = "https://apitiendavirtual-production.up.railway.app/carrito"
+// const carritoGet = "https://apitiendavirtual-production.up.railway.app/carrito/"
+// const carritoDelete = "https://apitiendavirtual-production.up.railway.app/carrito/"
+
+// //rutas mis_compras
+// const comprasGetAndPost = "https://apitiendavirtual-production.up.railway.app/compras"
+// const comprasGetAndDelete = "https://apitiendavirtual-production.up.railway.app/compras/"
 
 //rutas exportadas
 export default baseurl
